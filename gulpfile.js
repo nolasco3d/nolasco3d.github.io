9ad411f4c5fd541c8c8b9ad29f2e003d8117d0c8
André Nolasco <nolasco3d@gmail.com>
2019-11-25 18:56:38 -0300

    New structure available for all device

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
-// Imports functions from Gulp
-const { 
+const {
     task,
     series,
     parallel,
@@ -8,19 +7,15 @@ const {
     dest
 } = require('gulp');
 
-// Variables and Loaders
 let sass        = require('gulp-sass'),
     urlAdjust   = require('gulp-css-url-adjuster'),
-    // babel       = require('gulp-babel'),
     BrowserSync = require('browser-sync').create(),
     reload      = BrowserSync.reload,
-    fonts_path  = 'public/assets/fonts',
-    // images_path = 'public/assets/images',
-    css_path    = 'public/assets/css'
-    // rename  = require('gulp-rename'),
+    // fonts_path  = 'public/assets/fonts',
+    css_path  = 'public/assets/css'
 ;
 
-// Compile/Process Sass
+// Compile/process SCSS
 function compile_sass() {
     return src(['src/assets/scss/*.scss'])
     .pipe(sass())
@@ -31,35 +26,20 @@ function compile_sass() {
     .pipe(BrowserSync.stream());
 }
 
-// Create Server and Live Reload
+
 function watch_files() {
     BrowserSync.init({
         server: {
             baseDir: './public'
         }
     });
-    
-    // Watch SASS/SCSS files and Reload
+
+    // Watch SCSS files and Reload
     watch('src/assets/scss/**/*.scss', compile_sass);
+
     // Watch HTML files and Reload
     watch('./public/*.html').on('change', reload);
 }
 
-
-// Testing FontAwesome copy
-// function fontAwesome(done) {
-//     // Files to public fulder
-//     src(['node_modules/@fortawesome/fontawesome-free/webfonts'])
-//     .pipe(dest(fonts_path + '/webfonts'));
-    
-//     // Files to src
-//     src([
-//         'node_modules/@fortawesome/fontawesome-free/scss/**/*.scss'
-//     ])
-//     .pipe(dest('src/assets/scss/fontawesome'));
-
-//     done();
-// }
-
-// Define default task
-task('default', series( compile_sass, watch_files));
+// Default task
+task('default', series(compile_sass, watch_files));
