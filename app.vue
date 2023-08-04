d3bfd92201824d138ea260014ab990cbf8581f73
andre.castro <andre.castro@defensoria.mg.def.br>
2023-08-04 09:44:16 -0300

    Project restructuring

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="container mx-auto flex flex-col justify-center items-center h-screen">
+    <h1>Hello World!</h1>
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  tailwindcss: {},
+  googleFonts: {
+    families: {
+      'Fira+Mono': true
+    }
+  }
+}
+
