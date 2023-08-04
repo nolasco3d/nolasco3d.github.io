0f423d262ebdca700348741513e5e06775455cf9
nolasco3d <nolasco3d@gmail.com>
2023-08-04 17:48:54 +0000

    Deploying to gh-pages from @ nolasco3d/nolasco3d.github.io@f4f19bcf5b9439ca53579655d8d66f9dbdd1b6b4 🚀

--- _nuxt/ProseTable.1961f517.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,o as r,c as t,M as s}from"./entry.7c882538.js";const a={};function c(e,n){return r(),t("table",null,[s(e.$slots,"default")])}const _=o(a,[["render",c]]);export{_ as default};
